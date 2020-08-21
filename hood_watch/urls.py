e3f95587f7dd4117adbf3fdab2f430221eca4ab1
mishael254 <mishaelmishantoz@gmail.com>
2020-08-21 16:32:05 +0300

    remove logout url

--- hood_watch/urls.py
+++ hood_watch/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('hood.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+     
     
 ]
